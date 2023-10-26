3813bcda0c49da16ce6ce6ffaef65b69a9cd650f
Steve Rodrigue <stev.rodr@gmail.com>
2023-10-26 15:10:57 -0400

    Removed unnecessary code

--- domain/identities/profiles/requests/sdk.go
+++
@@ -1,9 +0,0 @@
-package requests
-
-import "github.com/steve-care-software/steve/domain/accounts/identities/profiles/authorizations"
-
-// Request represents an authorization request
-type Request interface {
-	Name() string
-	Authorization() authorizations.Authorization
-}
