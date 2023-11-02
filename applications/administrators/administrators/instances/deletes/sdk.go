3ffe0ca46e76403a8d488aa4cf30d91f46fdfe77
Steve Rodrigue <stev.rodr@gmail.com>
2023-11-02 02:39:56 -0400

    Added builder interfaces

--- applications/administrators/administrators/instances/deletes/sdk.go
+++ applications/administrators/administrators/instances/deletes/sdk.go
@@ -1,9 +1,17 @@
 package deletes
 
 import (
+	"github.com/steve-care-software/steve/domain/accounts/administrators"
 	executions "github.com/steve-care-software/steve/domain/commands/executions/administrators/instances/deletes"
 	inputs "github.com/steve-care-software/steve/domain/commands/inputs/administrators/administrators/instances/contents/deletes"
 )
+
+// Builder represents an application builder
+type Builder interface {
+	Create() Builder
+	WithInstance(instance administrators.Administrator) Builder
+	Now() (Application, error)
+}
 
 // Application represents the delete application
 type Application interface {
--- applications/administrators/administrators/instances/fetches/sdk.go
+++ applications/administrators/administrators/instances/fetches/sdk.go
@@ -1,9 +1,17 @@
 package fetches
 
 import (
+	"github.com/steve-care-software/steve/domain/accounts/administrators"
 	executions "github.com/steve-care-software/steve/domain/commands/executions/administrators/instances/fetches"
 	inputs "github.com/steve-care-software/steve/domain/commands/inputs/administrators/administrators/instances/contents/fetches"
 )
+
+// Builder represents an application builder
+type Builder interface {
+	Create() Builder
+	WithInstance(instance administrators.Administrator) Builder
+	Now() (Application, error)
+}
 
 // Application represents the fetch application
 type Application interface {
