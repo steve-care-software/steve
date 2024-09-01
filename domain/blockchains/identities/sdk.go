6a88b1f55b42b67b203d16998bef6401bed35795
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-01 16:00:21 -0400

    Added the flags in the identity

--- domain/blockchains/identities/sdk.go
+++ domain/blockchains/identities/sdk.go
@@ -2,10 +2,14 @@ package identities
 
 import (
 	"crypto/ed25519"
+
+	"github.com/steve-care-software/steve/domain/hash"
 )
 
 // Identity represents an identity
 type Identity interface {
 	Name() string
 	PK() ed25519.PrivateKey
+	HasFlags() bool
+	Flags() []hash.Hash
 }
