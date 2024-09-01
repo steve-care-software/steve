960156fd89b8608455cbc5074b53dea78e0fa20a
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-01 10:18:59 -0400

    Added the transpile application interface

---
+++ applications/transpiles/sdk.go
@@ -0,0 +1,12 @@
+package transpiles
+
+import (
+	"github.com/steve-care-software/steve/domain/programs"
+	"github.com/steve-care-software/steve/domain/programs/grammars"
+	"github.com/steve-care-software/steve/domain/transpiles"
+)
+
+// Application represents the transpile application
+type Application interface {
+	Execute(source programs.Program, target grammars.Grammar, bridge transpiles.Transpile) ([]byte, error)
+}
