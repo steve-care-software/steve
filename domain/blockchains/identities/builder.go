1d06f11f2738bc1daa34088aa858bdda8b9d9932
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-04 10:33:15 -0400

    Added the identity structs

---
+++ domain/blockchains/identities/builder.go
@@ -0,0 +1,68 @@
+package identities
+
+import (
+	"crypto/ed25519"
+	"errors"
+
+	"github.com/steve-care-software/steve/domain/hash"
+)
+
+type builder struct {
+	name  string
+	pk    ed25519.PrivateKey
+	flags []hash.Hash
+}
+
+func createBuilder() Builder {
+	out := builder{
+		name:  "",
+		pk:    nil,
+		flags: nil,
+	}
+
+	return &out
+}
+
+// Create initializes the builder
+func (app *builder) Create() Builder {
+	return createBuilder()
+}
+
+// WithName adds a name to the builder
+func (app *builder) WithName(name string) Builder {
+	app.name = name
+	return app
+}
+
+// WithPK adds a pk to the builder
+func (app *builder) WithPK(pk ed25519.PrivateKey) Builder {
+	app.pk = pk
+	return app
+}
+
+// WithFlags add flags to the builder
+func (app *builder) WithFlags(flags []hash.Hash) Builder {
+	app.flags = flags
+	return app
+}
+
+// Now builds a new Identity instance
+func (app *builder) Now() (Identity, error) {
+	if app.name == "" {
+		return nil, errors.New("the name is mandatory in order to build an Identity instance")
+	}
+
+	if app.pk == nil {
+		return nil, errors.New("the pk is mandatory in order to build an Identity instance")
+	}
+
+	if app.flags != nil && len(app.flags) <= 0 {
+		app.flags = nil
+	}
+
+	if app.flags != nil {
+		return createIdentityWithFlags(app.name, app.pk, app.flags), nil
+	}
+
+	return createIdentity(app.name, app.pk), nil
+}
--- domain/blockchains/identities/sdk.go
+++ domain/blockchains/identities/sdk.go
@@ -6,6 +6,20 @@ import (
 	"github.com/steve-care-software/steve/domain/hash"
 )
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents the builder
+type Builder interface {
+	Create() Builder
+	WithName(name string) Builder
+	WithPK(pk ed25519.PrivateKey) Builder
+	WithFlags(flags []hash.Hash) Builder
+	Now() (Identity, error)
+}
+
 // Identity represents an identity
 type Identity interface {
 	Name() string
