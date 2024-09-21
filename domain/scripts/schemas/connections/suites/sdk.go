a903a209481f4a2a8ba07bce4a47e411573edeac
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-20 22:30:10 -0400

    Added the suites structs

--- domain/scripts/schemas/connections/suites/sdk.go
+++ domain/scripts/schemas/connections/suites/sdk.go
@@ -2,10 +2,42 @@ package suites
 
 import "github.com/steve-care-software/steve/domain/hash"
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// NewSuiteBuilder creates a new suite builder
+func NewSuiteBuilder() SuiteBuilder {
+	hashAdapter := hash.NewAdapter()
+	return createSuiteBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents the suites builder
+type Builder interface {
+	Create() Builder
+	WithList(list []Suite) Builder
+	Now() (Suites, error)
+}
+
 // Suites represents suites
 type Suites interface {
 	Hash() hash.Hash
 	List() []Suite
+}
+
+// SuiteBuilder represents the suite builder
+type SuiteBuilder interface {
+	Create() SuiteBuilder
+	WithName(name string) SuiteBuilder
+	WithPath(path []string) SuiteBuilder
+	IsFail() SuiteBuilder
+	Now() (Suite, error)
 }
 
 // Suite represents a test suite
