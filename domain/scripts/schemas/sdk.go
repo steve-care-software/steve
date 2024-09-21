e93abf4cb4417eecfcfb5661e1fbee5e14493a5b
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-21 08:51:42 -0400

    Added the schema structs

--- domain/scripts/schemas/sdk.go
+++ domain/scripts/schemas/sdk.go
@@ -1,12 +1,32 @@
 package schemas
 
 import (
-	"github.com/steve-care-software/steve/domain/graphs/connections"
-	"github.com/steve-care-software/steve/domain/graphs/points"
 	"github.com/steve-care-software/steve/domain/hash"
 	"github.com/steve-care-software/steve/domain/scripts/schemas/compensations"
+	"github.com/steve-care-software/steve/domain/scripts/schemas/connections"
+	"github.com/steve-care-software/steve/domain/scripts/schemas/points"
 	"github.com/steve-care-software/steve/domain/scripts/schemas/roles"
 )
+
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents the builder
+type Builder interface {
+	Create() Builder
+	WithName(name string) Builder
+	WithVersion(version uint) Builder
+	WithPoints(points points.Points) Builder
+	WithConnections(connections connections.Connections) Builder
+	WithRole(role roles.Role) Builder
+	WithCompensation(compensation compensations.Compensation) Builder
+	Now() (Schema, error)
+}
 
 // Schema represents the schema
 type Schema interface {
