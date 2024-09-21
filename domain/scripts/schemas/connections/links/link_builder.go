50ea4c853a5dd30f7ed0e780cf72c2c1f714bec2
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-20 22:20:35 -0400

    Added the links structs

---
+++ domain/scripts/schemas/connections/links/link_builder.go
@@ -0,0 +1,67 @@
+package links
+
+import (
+	"errors"
+
+	"github.com/steve-care-software/steve/domain/hash"
+	"github.com/steve-care-software/steve/domain/scripts/schemas/points"
+)
+
+type linkBuilder struct {
+	hashAdapter hash.Adapter
+	origin      points.Point
+	target      points.Point
+}
+
+func createLinkBuilder(
+	hashAdapter hash.Adapter,
+) LinkBuilder {
+	out := linkBuilder{
+		hashAdapter: hashAdapter,
+		origin:      nil,
+		target:      nil,
+	}
+
+	return &out
+}
+
+// Create initializes the builder
+func (app *linkBuilder) Create() LinkBuilder {
+	return createLinkBuilder(
+		app.hashAdapter,
+	)
+}
+
+// WithOrigin adds an origin to the builder
+func (app *linkBuilder) WithOrigin(origin points.Point) LinkBuilder {
+	app.origin = origin
+	return app
+}
+
+// WithTarget adds a target to the builder
+func (app *linkBuilder) WithTarget(target points.Point) LinkBuilder {
+	app.target = target
+	return app
+}
+
+// Now builds a new Link instance
+func (app *linkBuilder) Now() (Link, error) {
+	if app.origin == nil {
+		return nil, errors.New("the origin is mandatory in order to build a Link instance")
+	}
+
+	if app.target == nil {
+		return nil, errors.New("the target is mandatory in order to build a Link instance")
+	}
+
+	pHash, err := app.hashAdapter.FromMultiBytes([][]byte{
+		app.origin.Hash().Bytes(),
+		app.target.Hash().Bytes(),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return createLink(*pHash, app.origin, app.target), nil
+}
