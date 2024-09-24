81657fba09875df308fba903ce9d23e8e17b9510
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-24 10:17:04 -0400

    Added the constant structs

--- domain/scripts/specifics/grammars/constants/sdk.go
+++ domain/scripts/specifics/grammars/constants/sdk.go
@@ -6,10 +6,42 @@ import (
 	"github.com/steve-care-software/steve/domain/scripts/specifics/grammars/constants/tokens"
 )
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// NewConstantBuilder creates a new constant builder
+func NewConstantBuilder() ConstantBuilder {
+	hashAdapter := hash.NewAdapter()
+	return createConstantBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents the constants builder
+type Builder interface {
+	Create() Builder
+	WithList(list []Constant) Builder
+	Now() (Constants, error)
+}
+
 // Constants represents constants
 type Constants interface {
 	Hash() hash.Hash
 	List() []Constant
+}
+
+// ConstantBuilder represents the constant builder
+type ConstantBuilder interface {
+	Create() ConstantBuilder
+	WithName(name string) ConstantBuilder
+	WithTokens(tokens tokens.Tokens) ConstantBuilder
+	WithSuites(suites suites.Suites) ConstantBuilder
+	Now() (Constant, error)
 }
 
 // Constant represents a constant
