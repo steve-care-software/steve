949cf1f51af3e55839b3d5af7584dfc4acea2876
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-25 13:37:19 -0400

    Added the engine structs

--- domain/scripts/specifics/programs/calls/functions/sdk.go
+++ domain/scripts/specifics/programs/calls/functions/sdk.go
@@ -1,9 +1,13 @@
 package functions
 
-import "github.com/steve-care-software/steve/domain/scripts/specifics/programs/calls/functions/parameters"
+import (
+	"github.com/steve-care-software/steve/domain/hash"
+	"github.com/steve-care-software/steve/domain/scripts/specifics/programs/calls/functions/parameters"
+)
 
 // Function represents a func call
 type Function interface {
+	Hash() hash.Hash
 	Name() string
 	Parameters() parameters.Parameters
 	IsEngine() bool
