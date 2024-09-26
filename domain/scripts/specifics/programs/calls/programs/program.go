48db9a90f7943ebc763aeae320f08603cd25f89d
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-25 21:26:00 -0400

    Added the program structs

---
+++ domain/scripts/specifics/programs/calls/programs/program.go
@@ -0,0 +1,38 @@
+package programs
+
+import "github.com/steve-care-software/steve/domain/hash"
+
+type program struct {
+	hash  hash.Hash
+	name  string
+	input string
+}
+
+func createProgram(
+	hash hash.Hash,
+	name string,
+	input string,
+) Program {
+	out := program{
+		hash:  hash,
+		name:  name,
+		input: input,
+	}
+
+	return &out
+}
+
+// Hash returns the hash
+func (obj *program) Hash() hash.Hash {
+	return obj.hash
+}
+
+// Name returns the name
+func (obj *program) Name() string {
+	return obj.name
+}
+
+// Input returns the input
+func (obj *program) Input() string {
+	return obj.input
+}
--- domain/scripts/specifics/programs/calls/programs/sdk.go
+++ domain/scripts/specifics/programs/calls/programs/sdk.go
@@ -1,7 +1,28 @@
 package programs
 
+import (
+	"github.com/steve-care-software/steve/domain/hash"
+)
+
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents the program builder
+type Builder interface {
+	Create() Builder
+	WithName(name string) Builder
+	WithInput(input string) Builder
+	Now() (Program, error)
+}
+
 // Program represents a program call
 type Program interface {
+	Hash() hash.Hash
 	Name() string
 	Input() string
 }
