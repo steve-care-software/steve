510cdeba7aab729ce881d4013b117e171862ffa8
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-25 21:46:30 -0400

    Separated the container intrfaces

--- domain/scripts/specifics/programs/containers/sdk.go
+++ domain/scripts/specifics/programs/containers/sdk.go
@@ -1,28 +1,51 @@
 package containers
 
+import (
+	"github.com/steve-care-software/steve/domain/hash"
+	"github.com/steve-care-software/steve/domain/scripts/specifics/programs/containers/kinds"
+)
+
+const (
+	// FlagSingle represents the flag single
+	FlagSingle (uint8) = iota
+
+	// FlagVector represents the flag vector
+	FlagVector
+
+	// FlagList represents the flag list
+	FlagList
+
+	// FlagSet represents the flag set
+	FlagSet
+
+	// FlagSortedSet represents the flag sorted_set
+	FlagSortedSet
+)
+
+// Builder represents the containers builder
+type Builder interface {
+	Create() Builder
+	WithList(list []Container) Builder
+	Now() (Containers, error)
+}
+
 // Containers represents containers
 type Containers interface {
+	Hash() hash.Hash
 	List() []Container
+}
+
+// ContainerBuilder represents the container builder
+type ContainerBuilder interface {
+	Create() ContainerBuilder
+	WithFlag(flag uint8) ContainerBuilder
+	WithKind(kind kinds.Kind) ContainerBuilder
+	Now() (Container, error)
 }
 
 // Container represents a container
 type Container interface {
+	Hash() hash.Hash
 	Flag() uint8 // single, vector, list, set, sorted_set
-	Kind() Kind
-}
-
-// Kind represents a kind
-type Kind interface {
-	IsNumeric() bool
-	Numeric() NumericKind
-	IsEngine() bool
-	Engine() *uint8 // path, routes, route
-	IsRemaining() bool
-	Remaining() *uint8 // string, bool
-}
-
-// NumericKind represents a numeric kind
-type NumericKind interface {
-	Flag() uint8 // uint, int, float
-	Size() uint8 // 8, 16, 32, 64
+	Kind() kinds.Kind
 }
