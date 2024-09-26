171506d7b9fe0bad6e85f92c44dbcb83133adfa7
Steve Rodrigue <stev.rodr@gmail.com>
2024-09-26 09:39:37 -0400

    Finished the operations intrfaces

--- domain/scripts/specifics/programs/operations/sdk.go
+++ domain/scripts/specifics/programs/operations/sdk.go
@@ -5,9 +5,90 @@ import (
 	"github.com/steve-care-software/steve/domain/scripts/specifics/programs/values"
 )
 
+const (
+	// SingleSwordPlus represents the ++
+	SingleSwordPlus (uint8) = iota
+
+	// SingleSwordMinus represents the --
+	SingleSwordMinus
+)
+
+const (
+	// BitShiftLeft represents the bitshift left
+	BitShiftLeft (uint8) = iota
+
+	// BitShiftRight represents the bitshift right
+	BitShiftRight
+)
+
+const (
+	// StandardOperationPlus represents the +
+	StandardOperationPlus (uint8) = iota
+
+	// StandardOperationMinus represents the -
+	StandardOperationMinus
+
+	// StandardOperationMul represents the *
+	StandardOperationMul
+
+	// StandardOperationDiv represents the /
+	StandardOperationDiv
+
+	// StandardOperationMod represents the %
+	StandardOperationMod
+
+	// StandardOperationSmallerThan represents the <
+	StandardOperationSmallerThan
+
+	// StandardOperationSmallerThan represents the <=
+	StandardOperationSmallerThanOrEqual
+
+	// StandardOperationBiggerThan represents the >
+	StandardOperationBiggerThan
+
+	// StandardOperationBiggerThanOrEqual represents the >=
+	StandardOperationBiggerThanOrEqual
+
+	// StandardOperationEqual represents the ==
+	StandardOperationEqual
+
+	// StandardOperationNotEqual represents the !=
+	StandardOperationNotEqual
+
+	// StandardOperationAnd represents the &&
+	StandardOperationAnd
+
+	// StandardOperationOr represents the ||
+	StandardOperationOr
+
+	// StandardOperationNot represents the !
+	StandardOperationNot
+
+	// StandardOperationXor represents the xor
+	StandardOperationXor
+)
+
+// Builder represents the operations builder
+type Builder interface {
+	Create() Builder
+	WithList(list []Operation) Builder
+	Now() (Operations, error)
+}
+
 // Operations represents operations
 type Operations interface {
+	Hash() hash.Hash
 	List() Operation
+}
+
+// OperationBuilder represents the operation builder
+type OperationBuilder interface {
+	Create() OperationBuilder
+	WithStandardOperation(standardOperation StandardOperation) OperationBuilder
+	WithSingleSword(singleSword SingleSwordOperation) OperationBuilder
+	WithBitShift(bitshift BitShift) OperationBuilder
+	WithValue(value values.Value) OperationBuilder
+	Now() (Operation, error)
 }
 
 // Operation represents an operation
@@ -16,12 +97,8 @@ type Operations interface {
 // logical: and, or, not, xor
 type Operation interface {
 	Hash() hash.Hash
-	IsArithmetic() bool
-	Arithmetic() StandardOperation
-	IsRelational() bool
-	Relational() StandardOperation
-	IsLogical() bool
-	Logical() StandardOperation
+	IsStandard() bool
+	Standard() StandardOperation
 	IsSingleSword() bool
 	SingleSword() SingleSwordOperation
 	IsBitShift() bool
@@ -30,21 +107,49 @@ type Operation interface {
 	Value() values.Value
 }
 
+// BitShiftBuilder represents the bitshift builder
+type BitShiftBuilder interface {
+	Create() BitShiftBuilder
+	WithOperation(operation Operation) BitShiftBuilder
+	WithFlag(flag uint8) BitShiftBuilder
+	WithAmount(amount uint8) BitShiftBuilder
+	Now() (BitShift, error)
+}
+
 // BitShift represents a bitshift
 type BitShift interface {
+	Hash() hash.Hash
 	Operation() Operation
 	Flag() uint8 // <<, >>
 	Amount() uint8
 }
 
+// StandardOperationBuilder represents the standard operation builder
+type StandardOperationBuilder interface {
+	Create() StandardOperationBuilder
+	WithOperations(operations Operations) StandardOperationBuilder
+	WithFlag(flag uint8) StandardOperationBuilder
+	Now() (StandardOperation, error)
+}
+
 // StandardOperation represents a standard operation
 type StandardOperation interface {
+	Hash() hash.Hash
 	Operations() Operations
 	Flag() uint8
 }
 
+// SingleSwordOperationBuilder represents the single sword operation builder
+type SingleSwordOperationBuilder interface {
+	Create() SingleSwordOperationBuilder
+	WithVariable(variable string) SingleSwordOperationBuilder
+	WithFlag(flag uint8) SingleSwordOperationBuilder
+	Now() (SingleSwordOperation, error)
+}
+
 // SingleSwordOperation represents a single sword operation
 type SingleSwordOperation interface {
+	Hash() hash.Hash
 	Variable() string
 	Flag() uint8 // ++, --
 }
