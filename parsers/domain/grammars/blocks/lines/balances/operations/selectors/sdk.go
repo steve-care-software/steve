6e77043522dc339492a726f78945172c85c7d7ef
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-01 11:26:46 -0400

    Added the selector structs

--- parsers/domain/grammars/blocks/lines/balances/operations/selectors/sdk.go
+++ parsers/domain/grammars/blocks/lines/balances/operations/selectors/sdk.go
@@ -4,6 +4,11 @@ import (
 	"github.com/steve-care-software/steve/parsers/domain/grammars/blocks/lines/balances/operations/selectors/chains"
 )
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
 // Builder represents a selector builder
 type Builder interface {
 	Create() Builder
---
+++ parsers/domain/grammars/blocks/lines/balances/operations/selectors/selector.go
@@ -0,0 +1,30 @@
+package selectors
+
+import "github.com/steve-care-software/steve/parsers/domain/grammars/blocks/lines/balances/operations/selectors/chains"
+
+type selector struct {
+	chain chains.Chain
+	isNot bool
+}
+
+func createSelector(
+	chain chains.Chain,
+	isNot bool,
+) Selector {
+	out := selector{
+		chain: chain,
+		isNot: isNot,
+	}
+
+	return &out
+}
+
+// Chain returns the chain
+func (obj *selector) Chain() chains.Chain {
+	return obj.chain
+}
+
+// IsNot returns true if not, false otherwise
+func (obj *selector) IsNot() bool {
+	return obj.isNot
+}
