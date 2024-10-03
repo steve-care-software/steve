7f3029e6eb6ba52190bdd84e28a1ea8305bd6e2e
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-03 08:48:19 -0400

    Added the connection structs

--- databases/graphs/domain/languages/connections/sdk.go
+++ databases/graphs/domain/languages/connections/sdk.go
@@ -6,9 +6,35 @@ import (
 	"github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/suites"
 )
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// NewConnectionBuilder creates a new connection builder
+func NewConnectionBuilder() ConnectionBuilder {
+	return createConnectionBuilder()
+}
+
+// Builder represents the connections builder
+type Builder interface {
+	Create() Builder
+	WithList(list []Connection) Builder
+	Now() (Connections, error)
+}
+
 // Connections represents connections
 type Connections interface {
 	List() []Connection
+}
+
+// ConnectionBuilder represents the connection builder
+type ConnectionBuilder interface {
+	Create() ConnectionBuilder
+	WithHeader(header headers.Header) ConnectionBuilder
+	WithLinks(links links.Links) ConnectionBuilder
+	WithSuites(suites suites.Suites) ConnectionBuilder
+	Now() (Connection, error)
 }
 
 // Connection represents a connection
