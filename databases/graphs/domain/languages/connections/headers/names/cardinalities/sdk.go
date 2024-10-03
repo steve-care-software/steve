ad51d8e206b23997e894ed472d3387eb6115c7bd
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-03 06:25:39 -0400

    Added the cardinality structs

--- databases/graphs/domain/languages/connections/headers/names/cardinalities/sdk.go
+++ databases/graphs/domain/languages/connections/headers/names/cardinalities/sdk.go
@@ -1,5 +1,18 @@
 package cardinalities
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents the cardinality builder
+type Builder interface {
+	Create() Builder
+	WithMin(min uint) Builder
+	WithMax(max uint) Builder
+	Now() (Cardinality, error)
+}
+
 // Cardinality represents a cardinality
 type Cardinality interface {
 	Min() uint
