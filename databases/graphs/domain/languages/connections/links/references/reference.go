bb66fd644ae7877b173a6bc01f8094b2c3f1c06a
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-03 07:09:03 -0400

    Added the references structs

---
+++ databases/graphs/domain/languages/connections/links/references/reference.go
@@ -0,0 +1,52 @@
+package references
+
+import "github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/links/references/externals"
+
+type reference struct {
+	internal string
+	external externals.External
+}
+
+func createReferenceWithInternal(
+	internal string,
+) Reference {
+	return createReferenceInternally(internal, nil)
+}
+
+func createReferenceWithExternal(
+	external externals.External,
+) Reference {
+	return createReferenceInternally("", external)
+}
+
+func createReferenceInternally(
+	internal string,
+	external externals.External,
+) Reference {
+	out := reference{
+		internal: internal,
+		external: external,
+	}
+
+	return &out
+}
+
+// IsInternal returns true if there is an internal, false otherwise
+func (obj *reference) IsInternal() bool {
+	return obj.internal != ""
+}
+
+// Internal returns the internal, if any
+func (obj *reference) Internal() string {
+	return obj.internal
+}
+
+// IsExternal returns true if there is an external, false otherwise
+func (obj *reference) IsExternal() bool {
+	return obj.external != nil
+}
+
+// External returns the external, if any
+func (obj *reference) External() externals.External {
+	return obj.external
+}
---
+++ databases/graphs/domain/languages/connections/links/references/reference_builder.go
@@ -0,0 +1,51 @@
+package references
+
+import (
+	"errors"
+
+	"github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/links/references/externals"
+)
+
+type referenceBuilder struct {
+	internal string
+	external externals.External
+}
+
+func createReferenceBuilder() ReferenceBuilder {
+	out := referenceBuilder{
+		internal: "",
+		external: nil,
+	}
+
+	return &out
+}
+
+// Create initializes the builder
+func (app *referenceBuilder) Create() ReferenceBuilder {
+	return createReferenceBuilder()
+}
+
+// WithInternal adds an internal to the builder
+func (app *referenceBuilder) WithInternal(internal string) ReferenceBuilder {
+	app.internal = internal
+	return app
+}
+
+// WithExternal adds an external to the builder
+func (app *referenceBuilder) WithExternal(external externals.External) ReferenceBuilder {
+	app.external = external
+	return app
+}
+
+// Nwo builds a new Reference instance
+func (app *referenceBuilder) Now() (Reference, error) {
+	if app.internal != "" {
+		return createReferenceWithInternal(app.internal), nil
+	}
+
+	if app.external != nil {
+		return createReferenceWithExternal(app.external), nil
+	}
+
+	return nil, errors.New("the Reference is invalid")
+}
