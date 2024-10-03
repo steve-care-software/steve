6b4b28a9a0be7fa3951bcad103e95817eed65eba
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-03 07:15:24 -0400

    Added the link structs

---
+++ databases/graphs/domain/languages/connections/links/link.go
@@ -0,0 +1,30 @@
+package links
+
+import "github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/links/references"
+
+type link struct {
+	origin references.Reference
+	target references.Reference
+}
+
+func createLink(
+	origin references.Reference,
+	target references.Reference,
+) Link {
+	out := link{
+		origin: origin,
+		target: target,
+	}
+
+	return &out
+}
+
+// Origin returns the origin
+func (obj *link) Origin() references.Reference {
+	return obj.origin
+}
+
+// Target returns the target
+func (obj *link) Target() references.Reference {
+	return obj.target
+}
--- databases/graphs/domain/languages/connections/links/sdk.go
+++ databases/graphs/domain/languages/connections/links/sdk.go
@@ -2,9 +2,34 @@ package links
 
 import "github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/links/references"
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// NewLinkBuilder creates a new link builder
+func NewLinkBuilder() LinkBuilder {
+	return createLinkBuilder()
+}
+
+// Builder represents the links builder
+type Builder interface {
+	Create() Builder
+	WithList(list []Link) Builder
+	Now() (Links, error)
+}
+
 // Links represents links
 type Links interface {
 	List() []Link
+}
+
+// LinkBuilder represents the link builder
+type LinkBuilder interface {
+	Create() LinkBuilder
+	WithOrigin(origin references.Reference) LinkBuilder
+	WithTarget(target references.Reference) LinkBuilder
+	Now() (Link, error)
 }
 
 // Link represents a link
