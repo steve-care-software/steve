4bba2dee0a9ff414048b16d4b572898ba7d4f1f2
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-03 07:21:23 -0400

    Added the expectation structs

---
+++ databases/graphs/domain/languages/connections/suites/expectations/builder.go
@@ -0,0 +1,50 @@
+package expectations
+
+import (
+	"errors"
+
+	"github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/links/references"
+)
+
+type builder struct {
+	references references.References
+	isFail     bool
+}
+
+func createBuilder() Builder {
+	out := builder{
+		references: nil,
+		isFail:     false,
+	}
+
+	return &out
+}
+
+// Create initializes the builder
+func (app *builder) Create() Builder {
+	return createBuilder()
+}
+
+// WithReferences add references to the builder
+func (app *builder) WithReferences(references references.References) Builder {
+	app.references = references
+	return app
+}
+
+// IsFail flags the builder as fail
+func (app *builder) IsFail() Builder {
+	app.isFail = true
+	return app
+}
+
+// Now builds a new Expectation instance
+func (app *builder) Now() (Expectation, error) {
+	if app.references == nil {
+		return nil, errors.New("the references is mandatory in order to build an Expectation instance")
+	}
+
+	return createExpectation(
+		app.references,
+		app.isFail,
+	), nil
+}
--- databases/graphs/domain/languages/connections/suites/expectations/sdk.go
+++ databases/graphs/domain/languages/connections/suites/expectations/sdk.go
@@ -2,8 +2,21 @@ package expectations
 
 import "github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/links/references"
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents the expectation builder
+type Builder interface {
+	Create() Builder
+	WithReferences(references references.References) Builder
+	IsFail() Builder
+	Now() (Expectation, error)
+}
+
 // Expectation represents a suite expectation
 type Expectation interface {
-	References() references.Reference
+	References() references.References
 	IsFail() bool
 }
