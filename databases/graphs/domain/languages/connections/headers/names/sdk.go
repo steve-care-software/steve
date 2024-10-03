9d389df44acac5ad73c92ad9b16a7961e3c50ea1
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-03 06:33:36 -0400

    Added the name structs

--- databases/graphs/domain/languages/connections/headers/names/sdk.go
+++ databases/graphs/domain/languages/connections/headers/names/sdk.go
@@ -2,6 +2,19 @@ package names
 
 import "github.com/steve-care-software/steve/databases/graphs/domain/languages/connections/headers/names/cardinalities"
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents the name builder
+type Builder interface {
+	Create() Builder
+	WithName(name string) Builder
+	WithCardinality(cardinality cardinalities.Cardinality) Builder
+	Now() (Name, error)
+}
+
 // Name represents an header name
 type Name interface {
 	Name() string
