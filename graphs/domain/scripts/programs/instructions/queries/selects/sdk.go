7362cccc36262299dc560eec84d73ef3e3bf368f
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-10 15:26:55 -0400

    Added the select structs

--- graphs/domain/scripts/programs/instructions/queries/selects/sdk.go
+++ graphs/domain/scripts/programs/instructions/queries/selects/sdk.go
@@ -5,6 +5,20 @@ import (
 	"github.com/steve-care-software/steve/graphs/domain/scripts/schemas/connections/links/references/externals"
 )
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents the select builder
+type Builder interface {
+	Create() Builder
+	WithExternals(externals externals.Externals) Builder
+	WithCondition(condition conditions.Condition) Builder
+	IsDelete() Builder
+	Now() (Select, error)
+}
+
 // Select represents a select
 type Select interface {
 	IsDelete() bool
